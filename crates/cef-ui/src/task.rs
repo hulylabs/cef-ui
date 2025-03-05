351ca71a55e8d93fb11ba57705e9db3ffa581921
NikitaSkrynnik <nikita.skrynnik@xored.com>
2025-03-05 15:09:11 +1100

    add docs
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>

--- crates/cef-ui/src/task.rs
+++ crates/cef-ui/src/task.rs
@@ -4,7 +4,14 @@ use cef_ui_sys::{cef_post_task, cef_task_t};
 
 use crate::{RefCountedPtr, ThreadId, Wrappable, Wrapped, ref_counted_ptr};
 
+/// Implement this structure for asynchronous task execution. If the task is
+/// posted successfully and if the associated message loop is still running then
+/// the execute() function will be called on the target thread. If the task
+/// fails to post then the task object may be destroyed on the source thread
+/// instead of the target thread. For this reason be cautious when performing
+/// work in the task object destructor.
 pub trait CefTaskCallbacks: Send + Sync + 'static {
+    /// Method that will be executed on the target thread.
     fn execute(&mut self);
 }
 
@@ -24,6 +31,7 @@ impl CefTaskWrapper {
         Self(Box::new(delegate))
     }
 
+    /// Method that will be executed on the target thread.
     unsafe extern "C" fn execute(this: *mut cef_task_t) {
         let this: &mut Self = Wrapped::wrappable(this);
         this.0.execute();
@@ -45,6 +53,8 @@ impl Wrappable for CefTaskWrapper {
     }
 }
 
+/// Post a task for execution on the specified thread. Equivalent to using
+/// cef_task_runner_t::GetForThread(threadId)->PostTask(task).
 pub fn post_task(tid: ThreadId, task: CefTask) -> bool {
     unsafe { cef_post_task(tid.into(), task.into_raw()) != 0 }
 }
