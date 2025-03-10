af869f863743a4f480f375981ff764a81353c157
NikitaSkrynnik <nikita.skrynnik@xored.com>
2025-03-10 20:25:27 +1100

    impl cef_load_handler
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>

--- crates/cef-ui/src/types.rs
+++ crates/cef-ui/src/types.rs
@@ -9,7 +9,7 @@ use cef_ui_sys::{
     cef_touch_handle_state_flags_t_CEF_THS_FLAG_NONE,
     cef_touch_handle_state_flags_t_CEF_THS_FLAG_ORIENTATION,
     cef_touch_handle_state_flags_t_CEF_THS_FLAG_ORIGIN, cef_touch_handle_state_t,
-    cef_window_open_disposition_t, cef_zoom_command_t
+    cef_transition_type_t, cef_window_open_disposition_t, cef_zoom_command_t
 };
 use std::ffi::c_int;
 
@@ -3206,6 +3206,185 @@ impl From<&ThreadId> for cef_thread_id_t {
             ThreadId::ProcessLauncher => Self::TID_PROCESS_LAUNCHER,
             ThreadId::IO => Self::TID_IO,
             ThreadId::Renderer => Self::TID_RENDERER
+        }
+    }
+}
+
+/// Transition type for a request. Made up of one source value and 0 or more
+/// qualifiers.
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[repr(u32)]
+pub enum TransitionType {
+    /// Source is a link click or the JavaScript window.open function. This is
+    /// also the default value for requests like sub-resource loads that are not
+    /// navigations.
+    Link = 0,
+
+    /// Source is some other "explicit" navigation. This is the default value for
+    /// navigations where the actual type is unknown. See also
+    /// TT_DIRECT_LOAD_FLAG.
+    Explicit = 1,
+
+    /// Source is a subframe navigation. This is any content that is automatically
+    /// loaded in a non-toplevel frame. For example, if a page consists of several
+    /// frames containing ads, those ad URLs will have this transition type.
+    /// The user may not even realize the content in these pages is a separate
+    /// frame, so may not care about the URL.
+    AutoBookmark = 2,
+
+    /// Source is a subframe navigation explicitly requested by the user that will
+    /// generate new navigation entries in the back/forward list. These are
+    /// probably more important than frames that were automatically loaded in
+    /// the background because the user probably cares about the fact that this
+    /// link was loaded.
+    AutoSubframe = 3,
+
+    /// Source is a subframe navigation explicitly requested by the user that will
+    /// generate new navigation entries in the back/forward list. These are
+    /// probably more important than frames that were automatically loaded in
+    /// the background because the user probably cares about the fact that this
+    /// link was loaded.
+    ManualSubframe = 4,
+
+    /// User got to this page by typing in the URL bar and selecting an entry
+    /// that did not look like a URL.  For example, a match might have the URL
+    /// of a Google search result page, but appear like "Search Google for ...".
+    /// These are not quite the same as EXPLICIT navigations because the user
+    /// didn't type or see the destination URL. Chrome style only.
+    /// See also TT_KEYWORD.
+    Generated = 5,
+
+    /// This is a toplevel navigation. This is any content that is automatically
+    /// loaded in a toplevel frame.  For example, opening a tab to show the ASH
+    /// screen saver, opening the devtools window, opening the NTP after the safe
+    /// browsing warning, opening web-based dialog boxes are examples of
+    /// AUTO_TOPLEVEL navigations. Chrome style only.
+    AutoToplevel = 6,
+
+    /// Source is a form submission by the user. NOTE: In some situations
+    /// submitting a form does not result in this transition type. This can happen
+    /// if the form uses a script to submit the contents.
+    FormSubmit = 7,
+
+    /// Source is a "reload" of the page via the Reload function or by re-visiting
+    /// the same URL. NOTE: This is distinct from the concept of whether a
+    /// particular load uses "reload semantics" (i.e. bypasses cached data).
+    Reload = 8,
+
+    /// The url was generated from a replaceable keyword other than the default
+    /// search provider. If the user types a keyword (which also applies to
+    /// tab-to-search) in the omnibox this qualifier is applied to the transition
+    /// type of the generated url. TemplateURLModel then may generate an
+    /// additional visit with a transition type of TT_KEYWORD_GENERATED against
+    /// the url 'http://' + keyword. For example, if you do a tab-to-search
+    /// against wikipedia the generated url has a transition qualifer of
+    /// TT_KEYWORD, and TemplateURLModel generates a visit for 'wikipedia.org'
+    /// with a transition type of TT_KEYWORD_GENERATED. Chrome style only.
+    Keyword = 9,
+
+    /// Corresponds to a visit generated for a keyword. See description of
+    /// TT_KEYWORD for more details. Chrome style only.
+    KeywordGenerated = 10,
+
+    /// General mask defining the bits used for the source values.
+    SourceMask = 0xFF,
+
+    /// Qualifiers.
+    /// Any of the core values above can be augmented by one or more qualifiers.
+    /// These qualifiers further define the transition.
+
+    /// Attempted to visit a URL but was blocked.
+    BlockedFlag = 0x00800000,
+
+    /// Used the Forward or Back function to navigate among browsing history.
+    /// Will be ORed to the transition type for the original load.
+    ForwardBackFlag = 0x01000000,
+
+    /// Loaded a URL directly via CreateBrowser, LoadURL or LoadRequest.
+    DirectLoadFlag = 0x02000000,
+
+    /// User is navigating to the home page. Chrome style only.
+    HomePageFlag = 0x04000000,
+
+    /// The transition originated from an external application; the exact
+    /// definition of this is embedder dependent. Chrome style only.
+    FromApiFlag = 0x08000000,
+
+    /// The beginning of a navigation chain.
+    ChainStartFlag = 0x10000000,
+
+    /// The last transition in a redirect chain.
+    ChainEndFlag = 0x20000000,
+
+    /// Redirects caused by JavaScript or a meta refresh tag on the page.
+    ClientRedirectFlag = 0x40000000,
+
+    /// Redirects sent from the server by HTTP headers.
+    ServerRedirectFlag = 0x80000000,
+
+    /// Used to test whether a transition involves a redirect.
+    IsRedirectMask = 0xC0000000,
+
+    /// General mask defining the bits used for the qualifiers.
+    QualifierMask = 0xFFFFFF00
+}
+
+impl From<cef_transition_type_t> for TransitionType {
+    fn from(value: cef_transition_type_t) -> Self {
+        match value {
+            cef_transition_type_t::TT_LINK => Self::Link,
+            cef_transition_type_t::TT_EXPLICIT => Self::Explicit,
+            cef_transition_type_t::TT_AUTO_BOOKMARK => Self::AutoBookmark,
+            cef_transition_type_t::TT_AUTO_SUBFRAME => Self::AutoSubframe,
+            cef_transition_type_t::TT_MANUAL_SUBFRAME => Self::ManualSubframe,
+            cef_transition_type_t::TT_GENERATED => Self::Generated,
+            cef_transition_type_t::TT_AUTO_TOPLEVEL => Self::AutoToplevel,
+            cef_transition_type_t::TT_FORM_SUBMIT => Self::FormSubmit,
+            cef_transition_type_t::TT_RELOAD => Self::Reload,
+            cef_transition_type_t::TT_KEYWORD => Self::Keyword,
+            cef_transition_type_t::TT_KEYWORD_GENERATED => Self::KeywordGenerated,
+            cef_transition_type_t::TT_SOURCE_MASK => Self::SourceMask,
+            cef_transition_type_t::TT_BLOCKED_FLAG => Self::BlockedFlag,
+            cef_transition_type_t::TT_FORWARD_BACK_FLAG => Self::ForwardBackFlag,
+            cef_transition_type_t::TT_DIRECT_LOAD_FLAG => Self::DirectLoadFlag,
+            cef_transition_type_t::TT_HOME_PAGE_FLAG => Self::HomePageFlag,
+            cef_transition_type_t::TT_FROM_API_FLAG => Self::FromApiFlag,
+            cef_transition_type_t::TT_CHAIN_START_FLAG => Self::ChainStartFlag,
+            cef_transition_type_t::TT_CHAIN_END_FLAG => Self::ChainEndFlag,
+            cef_transition_type_t::TT_CLIENT_REDIRECT_FLAG => Self::ClientRedirectFlag,
+            cef_transition_type_t::TT_SERVER_REDIRECT_FLAG => Self::ServerRedirectFlag,
+            cef_transition_type_t::TT_IS_REDIRECT_MASK => Self::IsRedirectMask,
+            cef_transition_type_t::TT_QUALIFIER_MASK => Self::QualifierMask
+        }
+    }
+}
+
+impl From<TransitionType> for cef_transition_type_t {
+    fn from(value: TransitionType) -> Self {
+        match value {
+            TransitionType::Link => cef_transition_type_t::TT_LINK,
+            TransitionType::Explicit => cef_transition_type_t::TT_EXPLICIT,
+            TransitionType::AutoBookmark => cef_transition_type_t::TT_AUTO_BOOKMARK,
+            TransitionType::AutoSubframe => cef_transition_type_t::TT_AUTO_SUBFRAME,
+            TransitionType::ManualSubframe => cef_transition_type_t::TT_MANUAL_SUBFRAME,
+            TransitionType::Generated => cef_transition_type_t::TT_GENERATED,
+            TransitionType::AutoToplevel => cef_transition_type_t::TT_AUTO_TOPLEVEL,
+            TransitionType::FormSubmit => cef_transition_type_t::TT_FORM_SUBMIT,
+            TransitionType::Reload => cef_transition_type_t::TT_RELOAD,
+            TransitionType::Keyword => cef_transition_type_t::TT_KEYWORD,
+            TransitionType::KeywordGenerated => cef_transition_type_t::TT_KEYWORD_GENERATED,
+            TransitionType::SourceMask => cef_transition_type_t::TT_SOURCE_MASK,
+            TransitionType::BlockedFlag => cef_transition_type_t::TT_BLOCKED_FLAG,
+            TransitionType::ForwardBackFlag => cef_transition_type_t::TT_FORWARD_BACK_FLAG,
+            TransitionType::DirectLoadFlag => cef_transition_type_t::TT_DIRECT_LOAD_FLAG,
+            TransitionType::HomePageFlag => cef_transition_type_t::TT_HOME_PAGE_FLAG,
+            TransitionType::FromApiFlag => cef_transition_type_t::TT_FROM_API_FLAG,
+            TransitionType::ChainStartFlag => cef_transition_type_t::TT_CHAIN_START_FLAG,
+            TransitionType::ChainEndFlag => cef_transition_type_t::TT_CHAIN_END_FLAG,
+            TransitionType::ClientRedirectFlag => cef_transition_type_t::TT_CLIENT_REDIRECT_FLAG,
+            TransitionType::ServerRedirectFlag => cef_transition_type_t::TT_SERVER_REDIRECT_FLAG,
+            TransitionType::IsRedirectMask => cef_transition_type_t::TT_IS_REDIRECT_MASK,
+            TransitionType::QualifierMask => cef_transition_type_t::TT_QUALIFIER_MASK
         }
     }
 }
