53f35b737ec234dde9dd09f144f8fcee6e100955
Nikita Skrynnik <nikita.skrynnik@xored.com>
2025-10-15 13:27:54 +0200

    disable downloading
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- crates/cef-ui/build.rs
+++ crates/cef-ui/build.rs
@@ -2,8 +2,8 @@ use anyhow::Result;
 use cef_ui_util::{download_and_extract_cef, get_cef_artifacts_dir};
 
 fn main() -> Result<()> {
-    let artifacts_dir = get_cef_artifacts_dir()?;
-    download_and_extract_cef(&artifacts_dir)?;
+    // let artifacts_dir = get_cef_artifacts_dir()?;
+    // download_and_extract_cef(&artifacts_dir)?;
 
     // Linker flags on x86_64 Linux.
     #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
