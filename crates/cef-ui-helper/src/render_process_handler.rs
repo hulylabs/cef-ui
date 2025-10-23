ae5c147595b04c94aa6d991507071660faf77d53
Nikita Skrynnik <nikita.skrynnik@xored.com>
2025-10-23 11:16:21 +0200

    add on_context_released to cef-ui-helper render_process_handler
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- crates/cef-ui-helper/src/render_process_handler.rs
+++ crates/cef-ui-helper/src/render_process_handler.rs
@@ -3,13 +3,14 @@ use crate::{
     v8_context::V8Context
 };
 use cef_ui_sys::{cef_browser_t, cef_frame_t, cef_render_process_handler_t, cef_v8context_t};
-use parking_lot::Mutex;
 use std::mem::zeroed;
 
 pub trait RenderProcessHandlerCallbacks: Send + Sync + 'static {
     fn on_web_kit_initialized(&mut self) {}
 
     fn on_context_created(&mut self, _browser: Browser, _frame: Frame, _context: V8Context) {}
+
+    fn on_context_released(&mut self, _browser: Browser, _frame: Frame, _context: V8Context) {}
 }
 
 ref_counted_ptr!(RenderProcessHandler, cef_render_process_handler_t);
@@ -20,18 +21,16 @@ impl RenderProcessHandler {
     }
 }
 
-struct RenderProcessHandlerWrapper(Mutex<Box<dyn RenderProcessHandlerCallbacks>>);
+struct RenderProcessHandlerWrapper(Box<dyn RenderProcessHandlerCallbacks>);
 
 impl RenderProcessHandlerWrapper {
     pub fn new<C: RenderProcessHandlerCallbacks>(delegate: C) -> Self {
-        Self(Mutex::new(Box::new(delegate)))
+        Self(Box::new(delegate))
     }
 
     unsafe extern "C" fn c_on_web_kit_initialized(this: *mut cef_render_process_handler_t) {
         let this: &mut Self = Wrapped::wrappable(this);
-        this.0
-            .lock()
-            .on_web_kit_initialized();
+        this.0.on_web_kit_initialized();
     }
 
     unsafe extern "C" fn c_on_context_created(
@@ -46,8 +45,22 @@ impl RenderProcessHandlerWrapper {
         let context = V8Context::from_ptr_unchecked(context);
 
         this.0
-            .lock()
             .on_context_created(browser, frame, context);
+    }
+
+    unsafe extern "C" fn c_on_context_released(
+        this: *mut cef_render_process_handler_t,
+        browser: *mut cef_browser_t,
+        frame: *mut cef_frame_t,
+        context: *mut cef_v8context_t
+    ) {
+        let this: &mut Self = Wrapped::wrappable(this);
+        let browser = Browser::from_ptr_unchecked(browser);
+        let frame = Frame::from_ptr_unchecked(frame);
+        let context = V8Context::from_ptr_unchecked(context);
+
+        this.0
+            .on_context_released(browser, frame, context);
     }
 }
 
@@ -63,7 +76,7 @@ impl Wrappable for RenderProcessHandlerWrapper {
                 on_browser_destroyed:        None,
                 get_load_handler:            None,
                 on_context_created:          Some(Self::c_on_context_created),
-                on_context_released:         None,
+                on_context_released:         Some(Self::c_on_context_released),
                 on_uncaught_exception:       None,
                 on_focused_node_changed:     None,
                 on_process_message_received: None
